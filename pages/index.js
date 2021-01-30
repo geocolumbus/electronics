da3df1c5c0c7d77f96c7e09b21d0e5ac7b641b6f
geocolumbus <geocolumbus@gmail.com>
2021-01-29 20:53:39 -0500

    Configured for basic single page app

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,46 @@
-import Head from 'next/head'
-import { Box } from '@material-ui/core'
+import { Box, Container, Grid } from '@material-ui/core'
+import { makeStyles } from '@material-ui/core/styles'
+
+const useStyles = makeStyles(theme => ({
+    container: {
+        maxWidth: 1080
+    },
+    header: {
+        backgroundColor: '#ffc',
+        padding: theme.spacing(2),
+        height: 100
+    },
+    menu: {
+        backgroundColor: '#cff',
+        height: 'calc(100vh - 200px)'
+    },
+    body: {
+        backgroundColor: '#fcf',
+        height: 'calc(100vh - 200px)'
+    },
+    footer: {
+        backgroundColor: '#ffc',
+        padding: theme.spacing(2),
+        height: 100
+    }
+}))
 
 export default function Home() {
-    return (
-        <>
-            <Head>
-                <title>Create Next App</title>
-                <link rel='icon' href='/favicon.ico' />
-            </Head>
 
-            <main>
-                <h1>
-          Electronics
-                </h1>
-                <Box>George</Box>
-            </main>
-        </>
+    const classes = useStyles()
+
+    return (
+        <Container className={classes.container}>
+            <Grid container spacing={0}>
+                <Grid item xs={12}>
+                    <Box className={classes.header}>Header</Box>
+                </Grid>
+                <Grid item xs={2}><Box className={classes.menu}>Menu</Box></Grid>
+                <Grid item xs={10}><Box className={classes.body}>Body</Box></Grid>
+                <Grid item xs={12}>
+                    <Box className={classes.footer}>Footer</Box>
+                </Grid>
+            </Grid>
+        </Container>
     )
 }
