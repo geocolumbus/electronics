f8804212a8b581a98036c0a082868c575e0318ab
geocolumbus <geocolumbus@gmail.com>
2021-02-02 08:59:45 -0500

    Fix typo

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import { makeStyles } from '@material-ui/core/styles'
 import { Container } from '@material-ui/core'
 import PanelScrew from '../components/atoms/PanelScrew'
 
-const useStyles = makeStyles(theme => (
+const useStyles = makeStyles(theme => ({
     container: {
         maxWidth: 1080
     }
